c7ca5fe3535971d582d86b383b3d342d1dacfd9b
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-03-27 16:46:51 +0100

    tailwind fix

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,6 +3,6 @@
 
 <template>
   <div>
-    <h1 class="text-2xl font-bold">Welcome to Laravel + Vue 3 + Inertia!</h1>
+    <h1 class="text-2xl font-bold text-red-500">Welcome to Laravel + Vue 3 + Inertia!</h1>
   </div>
 </template>
